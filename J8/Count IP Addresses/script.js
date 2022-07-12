d4256e4ee59ddaa7ab5e61c414031a97a4ddbdd9
Helen <99767950+HelenKrutskykh@users.noreply.github.com>
2022-07-13 02:18:48 +0300

    Add files via upload

---
+++ J8/Count IP Addresses/script.js
@@ -0,0 +1,24 @@
+// Count IP Addresses
+
+// Implement a function that receives two IPv4 addresses,
+// and returns the number of addresses between them (including the first one,
+// excluding the last one).
+
+// All inputs will be valid IPv4 addresses in the form of strings.
+// The last address will always be greater than the first one.
+
+// Examples : 
+// ipsBetween("10.0.0.0", "10.0.0.50") === 50;
+// ipsBetween("10.0.0.0", "10.0.1.0") === 256;
+// ipsBetween("20.0.0.10", "20.0.1.0") === 246.
+
+function ipsBetween(ip1, ip2) {
+  ip1 = ip1.split('.');
+  return ip2.split('.').reduce(function(count, num, i) {
+    return (count * 256) + Number(num) - Number(ip1[i])
+  }, 0);
+}
+ 
+console.log ( ipsBetween ("10.0.0.0", "10.0.0.50")) ;
+console.log ( ipsBetween ( "10.0.0.0", "10.0.1.0")) ;
+console.log ( ipsBetween ("20.0.0.10", "20.0.1.0")) ;
