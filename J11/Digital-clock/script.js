e8eba3a7117e8ef326ef2cb7be6961ecacd93477
Helen <99767950+HelenKrutskykh@users.noreply.github.com>
2022-08-04 19:27:55 +0300

    Add files via upload

--- J11/Digital-clock/script.js
+++ J11/Digital-clock/script.js
@@ -1,16 +1,16 @@
 function time() {
-  date = new Date();
+   let date = new Date();
 
   let month = date.toLocaleString( 'en-gb', {month: 'long', day: 'numeric'});
   //Метод toLocaleString() возвращает строку с языкозависимым представлением даты.
 
-  hr = date.getHours();
+ let hr = date.getHours();
   hr = (hr < 10) ? "0" + hr : hr;
 
-  mm = date.getMinutes();
+  let mm = date.getMinutes();
   mm = (mm < 10) ? "0" + mm : mm;
 
-  ss = date.getSeconds();
+  let ss = date.getSeconds();
   ss = (ss < 10) ? "0" + ss : ss;
 
   document.querySelector(".month").innerText = month;
