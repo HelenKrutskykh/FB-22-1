a7bdf23d767bef3a74a5c0f4d68e4b9311045741
Ksuyu <oksana.chukhalo@gmail.com>
2022-11-01 20:08:48 +0200

    const up

--- J20/script.js
+++ J20/script.js
@@ -8,31 +8,24 @@ const getNewUserInfo = () => {
 const displayUserInfo = (data) => {
   
   const userInfo = data.results[0];
-
-  document.querySelector("#displayUserPhoto").src = userInfo.picture.large;
-
   const userName = `${userInfo.name.first} ${userInfo.name.last}`;
-  document.querySelector("#displayUserName").innerText = userName;
-
   const userFullName = `${userInfo.name.title}. ${userInfo.name.first} ${userInfo.name.last}`;
-  document.querySelector("#displayUserFullName").innerText = userFullName;
-
   const userEmailAddress = userInfo.email;
-  document.querySelector("#displayUserEmail").innerText = userEmailAddress;
-
   const userPhoneNumber = userInfo.phone;
-  document.querySelector("#displayUserPhoneNumber").innerText = userPhoneNumber;
-
   const userGender = userInfo.gender;
-  document.querySelector("#displayUserGander").innerText = userGender;
-
-  let userDOB = new Date(userInfo.dob.date);
-  userDOB = `${userDOB.getDate()}/${userDOB.getMonth() + 1}/${userDOB.getFullYear()} (${userInfo.dob.age} years old)`
-  document.querySelector("#displayUserDOB").innerText = userDOB;
-
   const userAddress = `Street: ${userInfo.location.street.number} ${userInfo.location.street.name}, City: ${userInfo.location.city}, State: ${userInfo.location.state}, ${userInfo.location.country}.`;
+  
+  document.querySelector("#displayUserPhoto").src = userInfo.picture.large;
+  document.querySelector("#displayUserName").innerText = userName;
+  document.querySelector("#displayUserFullName").innerText = userFullName;
+  document.querySelector("#displayUserEmail").innerText = userEmailAddress;
+  document.querySelector("#displayUserPhoneNumber").innerText = userPhoneNumber;
+  document.querySelector("#displayUserGander").innerText = userGender;
+  document.querySelector("#displayUserDOB").innerText = userDOB;
   document.querySelector("#displayUserAddress").innerText = userAddress;
+  
+  let userDOB = new Date(userInfo.dob.date);
+  userDOB = `${userDOB.getDate()}/${userDOB.getMonth() + 1}/${userDOB.getFullYear()} (${userInfo.dob.age} years old)`;
 }
-
 
 getNewUserInfo();
